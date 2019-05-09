062431f6593b9336a6bf57f2789a6de57ec3bf2b
Tristan <tristan.salles@sydney.edu.au>
2019-05-10 09:51:05 +1000

    add visualisation functions based on lavavu

--- bioLEC-src/bioLEC/LEC.py
+++ bioLEC-src/bioLEC/LEC.py
@@ -23,6 +23,12 @@ import numpy as np
 import pandas as pd
 from mpi4py import MPI
 from skimage import graph
+from pyevtk.hl import gridToVTK
+
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from scipy.interpolate import RegularGridInterpolator
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -71,32 +77,36 @@ class landscapeConnectivity(object):
     """
 
     def __init__(self, filename=None, periodic=False, symmetric=False, sigmap=0.1, sigmav=None,
-                        connected=True, delimiter=r'\s+', header=None):
-
-        if self.rank == 0:
-            print('bioLEC - LANDSCAPE ELEVATIONAL CONNECTIVITY \n')
-
-        # Read DEM file
-        self.demfile = filename
-        df = pd.read_csv(self.demfile, sep=delimiter, engine='c',
-                               header=header, na_filter=False, dtype=np.float,
-                               low_memory=False)
-        X = df['X']
-        Y = df['Y']
-        Z = df['Z']
-        dx = ( X[1] - X[0] )
-        nx = int((X.max() - X.min())/dx+1)
-        ny = int((Y.max() - Y.min())/dx+1)
-        Z = Z.values.reshape(ny,nx)
-        del X
-        gc.collect()
-        del Y
-        gc.collect()
+                        connected=True, delimiter=',', header=0):
 
         # Set MPI communications
         self.comm = MPI.COMM_WORLD
         self.size = self.comm.Get_size()
         self.rank = self.comm.Get_rank()
+
+        if self.rank == 0:
+            print('bioLEC - LANDSCAPE ELEVATIONAL CONNECTIVITY')
+            print('-------------------------------------------\n')
+
+        # Read DEM file
+        self.demfile = filename
+        df = pd.read_csv(self.demfile, sep=',', engine='c',
+                               header=header, na_filter=False, dtype=np.float,
+                               low_memory=False)
+        X = df['X']
+        Y = df['Y']
+        Z = df['Z']
+        self.X = X.values
+        self.Y = Y.values
+        self.Z = Z.values
+        dx = ( X[1] - X[0] )
+        self.nx = int((X.max() - X.min())/dx+1)
+        self.ny = int((Y.max() - Y.min())/dx+1)
+        Z = Z.values.reshape(self.ny,self.nx)
+        del X
+        gc.collect()
+        del Y
+        gc.collect()
 
         self.connected = connected
         if periodic:
@@ -313,13 +323,13 @@ class landscapeConnectivity(object):
         for r in range(rsID[self.rank],reID[self.rank]):
             for c in range(0,self.nc):
                 if k%fout==0 and k>0:
-                    print('  + Processor {:4d} - Compute closeness between sites in {:.2f} s - completion: {:.2f} %'.format(self.rank,time.clock()-t1,k*100./steps))
+                    # print('  +  Processor {:4d} - Compute closeness between sites in {:.2f} s - completion: {:.2f} %'.format(self.rank,time.clock()-t1,k*100./steps))
+                    if self.rank == 0:
+                        print('  +  Compute closeness between sites in {:.2f} s - completion: {:.2f} %'.format(time.clock()-t1,k*100./steps))
                     t1 = time.clock()
                 localLEC += np.exp(-self._computeMinPath(r, c)/self.sigma2)
                 k += 1
 
-        del self.data
-        gc.collect()
         del startID
         gc.collect()
         del endID
@@ -349,22 +359,305 @@ class landscapeConnectivity(object):
 
         return
 
-    def writeLEC(self, filename='LECout.csv'):
+    def viewResult(self, imName=None, size=(9,5), fsize=11, cmap1=plt.cm.summer_r, cmap2=plt.cm.coolwarm, dpi=200):
         """
-        This function writes the computed landscape elevational connectivity array to a file.
+        This function plots and save in a figure the result of the LEC computation.
 
         Parameters
         ----------
-        filename : (string) output file name.
+        imName  : (string) image name with extension
+        size    : size of the image
+        fsize   : title font size
+        cmap1   : color map for elevation grid
+        cmap2   : color map for LEC grid
+        dpi     : resolution of the saved image
+        """
+
+        rcParams['figure.figsize'] = size
+        fig, (ax1, ax2) = plt.subplots(1,2)
+
+        fig.suptitle('Elevation grid and associated landscape elevational connectivity', fontsize=fsize)
+
+        # SUBPLOT 1
+        ax1.set_title('Elevation', fontsize=fsize-1)
+        im1 = ax1.imshow(np.flipud(self.data), vmin=self.data.min(), vmax=self.data.max(),
+                         cmap=cmap1, aspect='auto')
+
+        divider1 = make_axes_locatable(ax1)
+        cax1 = divider1.append_axes("right", size="5%", pad=0.05)
+        cbar1 = plt.colorbar(im1, cax=cax1)
+        cbar1.ax.tick_params(labelsize=fsize-3)
+
+        # Remove ticks from ax1
+        ax1.xaxis.set_visible(False)
+        ax1.yaxis.set_visible(False)
+
+
+        # SUBPLOT 2
+        ax2.set_title('LEC', fontsize=fsize-1)
+        im2 = ax2.imshow(np.flipud(self.LEC), vmin=self.LEC.min(), vmax=self.LEC.max(),
+                         cmap=cmap2, aspect='auto')
+
+        divider2 = make_axes_locatable(ax2)
+        cax2 = divider2.append_axes("right", size="5%", pad=0.05)
+        cbar2 = plt.colorbar(im2, cax=cax2)
+        cbar2.ax.tick_params(labelsize=fsize-3)
+
+        # Remove xticks from ax2
+        ax2.xaxis.set_visible(False)
+        ax2.yaxis.set_visible(False)
+
+        # Make space for title
+        plt.tight_layout()
+        plt.subplots_adjust(top=0.85)
+
+        if self.size == 1:
+            plt.show()
+
+        if imName is not None:
+            fig.savefig(imName, dpi=dpi, bbox_inches='tight')
+
+        plt.close(fig)
+
+        return
+
+    def viewElevFrequency(self, input=None, imName=None, nbins=80, size=(6,6), fsize=11, dpi=200):
+        """
+        This function plots and save in a figure the distribution of elevation.
+
+        Parameters
+        ----------
+        input   : (string) file name containing the dataset
+        imName  : (string) image name with extension
+        nbins   : number of bins for the histogram
+        size    : size of the image
+        fsize   : title font size
+        dpi     : resolution of the saved image
+        """
+
+        data = pd.read_csv(input)
+
+        ax = data.Z.plot(kind='hist', color='Blue', alpha=0.2, bins=nbins, xlim=(data.Z.min(),data.Z.max()))
+
+        ax.set_title('Elevation frequency as function of site elevation', fontsize=fsize)
+
+        plt.xlabel('Elevation', fontsize=fsize-1)
+        fig = data.Z.plot(kind='density', figsize=size, ax=ax, xlim=(data.Z.min(),data.Z.max()),
+                       linewidth=4, fontsize = fsize-3, secondary_y=True, y='Density').get_figure()
+
+        ax.set_ylabel('Frequency count', fontsize=fsize-1)
+
+        ax.tick_params(axis='x', labelsize=fsize-3)
+        ax.tick_params(axis='y', labelsize=fsize-3)
+
+        plt.ylabel('Density', fontsize=fsize-1)
+
+        if self.size == 1:
+            plt.show()
+
+        if imName is not None:
+            fig.savefig(imName, dpi=dpi, bbox_inches='tight')
+
+        plt.close(fig)
+        plt.close()
+
+        del data
+        gc.collect()
+
+        return
+
+    def viewLECFrequency(self, input=None, imName=None, size=(6,6), fsize=11, dpi=200):
+        """
+        This function plots and save in a figure the distribution of LEC with elevation.
+
+        Parameters
+        ----------
+        input   : (string) file name containing the dataset
+        imName  : (string) image name with extension
+        size    : size of the image
+        fsize   : title font size
+        dpi     : resolution of the saved image
+        """
+
+        data = pd.read_csv(input)
+
+        ff = data.plot(kind='scatter', x='Z', y='LEC', c='white', edgecolors='lightgray', figsize=size,
+                  xlim=(data['Z'].min(),data['Z'].max()), s=5)
+
+        ff.set_title('Landscape elevational connectivity as function of site elevation', fontsize=fsize)
+
+        plt.xlabel('Elevation', fontsize=fsize-1)
+        ff.set_ylabel('LEC', fontsize=fsize-1)
+        ff.tick_params(axis='x', labelsize=fsize-3)
+        ff.tick_params(axis='y', labelsize=fsize-3)
+
+
+        if self.size == 1:
+            plt.show()
+
+        if imName is not None:
+            fig = ff.get_figure()
+            fig.savefig(imName, dpi=dpi, bbox_inches='tight')
+
+        plt.close()
+
+        del data
+        gc.collect()
+
+        return
+
+    def viewLECZbar(self, input=None, imName=None, nbins=40, size=(6,6), fsize=11, dpi=200):
+        """
+        This function plots and save in a figure the distribution of LEC and elevation with elevation
+        as well as the standard deviation for LEC values for each bins.
+
+        Parameters
+        ----------
+        input   : (string) file name containing the dataset
+        imName  : (string) image name with extension
+        nbins   : number of bins for the histogram
+        size    : size of the image
+        fsize   : title font size
+        dpi     : resolution of the saved image
+        """
+
+        data = pd.read_csv(input)
+        n, _ = np.histogram(data.Z, bins=nbins)
+        sy, _ = np.histogram(data.Z, bins=nbins, weights=data.LEC)
+        sy2, _ = np.histogram(data.Z, bins=nbins, weights=data.LEC*data.LEC)
+        mean = sy / n
+        std = np.sqrt(sy2/n - mean*mean)
+
+        fig, ax = plt.subplots(figsize=size)
+
+        ax.set_title('Landscape elevational connectivity as function of site elevation with error bars', fontsize=fsize)
+
+        plt.plot((_[1:] + _[:-1])/2, mean,color='steelblue',zorder=2,linewidth=3)
+        plt.scatter(data.Z, data.LEC, c='w',edgecolors='lightgray', zorder=0,alpha=1.,s=5)
+        ax.set_xlim(data.Z.min(),data.Z.max())
+        ax.tick_params(axis='x', labelsize=fsize-3)
+        ax.tick_params(axis='y', labelsize=fsize-3)
+
+        plt.xlabel('Elevation', fontsize=fsize-1)
+        plt.ylabel('LEC', fontsize=fsize-1)
+
+        (_, caps, _) = plt.errorbar(
+            (_[1:] + _[:-1])/2, mean, yerr=std, fmt='-o', c='steelblue',markersize=4, capsize=3,zorder=1,linewidth=1.25)
+
+        for cap in caps:
+            cap.set_markeredgewidth(1)
+
+        ax2=ax.twinx()
+        data.Z.plot(kind='density',secondary_y=True, ax=ax2, xlim=(data.Z.min(),data.Z.max()),
+                       color='green', linewidth=3, zorder=0, fontsize = fsize-3)
+
+        plt.ylabel('Density', color='green', fontsize=fsize-1)
+        plt.tick_params(axis='x', labelsize=fsize-3)
+        plt.tick_params(axis='y', labelsize=fsize-3)
+
+        if self.size == 1:
+            plt.show()
+
+        if imName is not None:
+            fig.savefig(imName, dpi=dpi, bbox_inches='tight')
+
+        plt.close()
+
+        del data, _, n, sy, sy2, mean, std, caps
+        gc.collect()
+
+        return
+
+    def viewLECZFrequency(self, input=None, imName=None, size=(6,6), fsize=11, dpi=200):
+        """
+        This function plots and save in a figure the distribution of LEC and elevation with elevation.
+
+        Parameters
+        ----------
+        input   : (string) file name containing the dataset
+        imName  : (string) image name with extension
+        size    : size of the image
+        fsize   : title font size
+        dpi     : resolution of the saved image
+        """
+
+        data = pd.read_csv(input)
+
+        ax = data.plot(kind='scatter', x='Z', y='LEC', c='white', edgecolors='lightgray', figsize=size,
+                  xlim=(data['Z'].min(),data['Z'].max()), s=5)
+
+        ax.set_title('Landscape elevational connectivity as function of site elevation', fontsize=fsize)
+
+        plt.xlabel('Elevation', fontsize=fsize-1)
+        ax2=ax.twinx()
+        fig = data.Z.plot(kind='density',secondary_y=True, ax=ax2, xlim=(data['Z'].min(),data['Z'].max()),
+                       fontsize = fsize-3, linewidth=4).get_figure()
+
+        ax.set_ylabel('LEC', fontsize=fsize-1)
+        ax.tick_params(axis='x', labelsize=fsize-3)
+        ax.tick_params(axis='y', labelsize=fsize-3)
+
+        plt.ylabel('Density', fontsize=fsize-1)
+
+        if self.size == 1:
+            plt.show()
+
+        if imName is not None:
+            fig.savefig(imName, dpi=dpi, bbox_inches='tight')
+
+        plt.close(fig)
+        plt.close()
+
+        del data
+        gc.collect()
+
+        return
+
+
+    def writeLEC(self, filename='LECout'):
+        """
+        This function writes the computed landscape elevational connectivity array in a CSV and create
+        a VTK visualisation file (.vts)
+
+        Parameters
+        ----------
+        filename : (string) output file name without format extension.
         """
 
         time0 = time.clock()
         if self.rank == 0:
-            df = pd.DataFrame({'LEC':self.LEC[self.nr0:self.nr1,self.nc0:self.nc1].flatten()})
-            df.to_csv(filename, columns=['LEC'], sep=',', index=False , header=1)
+            XX = self.X.flatten()
+            YY = self.Y.flatten()
+            ZZ = self.Z.flatten()
+            LL = self.LEC[self.nr0:self.nr1,self.nc0:self.nc1].flatten()
+            df = pd.DataFrame({'X':XX,'Y':YY,'Z':ZZ,'LEC':LL})
+            df.to_csv(filename+'.csv', columns=['X', 'Y', 'Z', 'LEC'], sep=',', index=False , header=1)
 
-        del self.LEC
-        gc.collect()
+            vtkfile = filename
+            XX = XX.reshape(self.ny,self.nx)
+            YY = YY.reshape(self.ny,self.nx)
+            ZZ = ZZ.reshape(self.ny,self.nx)
+            LL = LL.reshape(self.ny,self.nx)
+            Xv = np.zeros((self.ny,self.nx,2))
+            Yv = np.zeros((self.ny,self.nx,2))
+            Zv = np.zeros((self.ny,self.nx,2))
+            LECv = np.zeros((self.ny,self.nx,2))
+            normLEC = np.zeros((self.ny,self.nx,2))
+
+            Xv[:,:,0] = XX
+            Yv[:,:,0] = YY
+            Zv[:,:,0] = ZZ
+            LECv[:,:,0] = LL
+            normLEC[:,:,0] = LL/LL.max()
+
+            Xv[:,:,1] = XX
+            Yv[:,:,1] = YY
+            Zv[:,:,1] = ZZ
+            LECv[:,:,1] = LL
+            normLEC[:,:,1] = LL/LL.max()
+
+            gridToVTK(vtkfile, Xv, Yv, Zv, pointData = {"elevation" : Zv, "LEC" :LECv, "nLEC" :normLEC})
+            del XX,YY, ZZ, LL, Xv, Yv, Zv, LECv, normLEC
 
         self.comm.Barrier()
 
--- bioLEC-src/bioLEC/Notebooks/notebooks/runLEC.py
+++ bioLEC-src/bioLEC/Notebooks/notebooks/runLEC.py
@@ -1,23 +1,51 @@
-import time
-import numpy as np
-import pandas as pd
-from skimage import graph
-from LECmetrics import LEC
+import argparse
+#from bioLEC import LEC
+import bbb as LEC
 
-import warnings
-warnings.filterwarnings('ignore')
-warnings.simplefilter(action = "ignore", category = FutureWarning)
+# Parsing command line arguments
+parser = argparse.ArgumentParser(description='This is a simple entry to run bioLEC package from python.',add_help=True)
 
-biodiv = LEC.LEC(filename='dataset/dem.csv',periodic=False,symmetric=False,
-                 sigmap=0.1,sigmav=None,connected=False,
-                 delimiter=',',header=0)
+# Required
+parser.add_argument('-i','--input', help='Input file name (csv file)',required=True)
+parser.add_argument('-o','--output',help='Output file name without extension', required=True)
 
-time0 = time.clock()
-biodiv.computeLEC(fout=500)
+# Optional
+parser.add_argument('-p','--periodic',help='True/false option for periodic boundary conditions', required=False, action="store_true", default=False)
+parser.add_argument('-s','--symmetric',help='True/false option for symmetric boundary conditions', required=False, action="store_true", default=False)
+parser.add_argument('-w','--width',help='Float option for species niche width percentage', required=False, action="store_true", default=0.1)
+parser.add_argument('-f','--fix',help='Float option for species niche width fix values', required=False, action="store_true", default=None)
+parser.add_argument('-c','--connected',help='True/false option for computing the path based on the diagonal moves as well as the axial ones', required=False, action="store_true", default=True)
+parser.add_argument('-t','--top',help='Header lines in the elevation grid', required=False, action="store_true", default=0)
+parser.add_argument('-n','--nout',help='Number for output frequency during run', required=False, action="store_true", default=500)
+parser.add_argument('-d','--delimiter',help='String for elevation grid csv delimiter', required=False,action="store_true",default=',')
+parser.add_argument('-v','--verbose',help='True/false option for verbose', required=False,action="store_true",default=False)
+
+
+args = parser.parse_args()
+if args.verbose:
+  print("Required arguments: ")
+  print("   + Input file: {}".format(args.input))
+  print("   + Output file without extension: {}".format(args.output))
+  print("\nOptional arguments: ")
+  print("   + Periodic boundary conditions for the elevation grid: {}".format(args.periodic))
+  print("   + Symmetric boundary conditions for the elevation grid: {}".format(args.symmetric))
+  print("   + Species niche width percentage based on elevation extent: {}".format(args.width))
+  print("   + Species niche width based on elevation extent: {}".format(args.fix))
+  print("   + Computes the path based on the diagonal moves as well as the axial ones: {}".format(args.connected))
+  print("   + Elevation grid csv delimiter: {}".format(args.delimiter))
+  print("   + Number of header lines: {}".format(args.top))
+  print("   + Number for output frequency: {}\n".format(args.nout))
+
+biodiv = LEC.landscapeConnectivity(filename=args.input,periodic=args.periodic,symmetric=args.symmetric,
+                                    sigmap=args.width,sigmav=args.fix,connected=args.connected,
+                                    delimiter=args.delimiter,header=args.top)
+
+biodiv.computeLEC(args.nout)
+
+biodiv.writeLEC(args.output)
 if biodiv.rank == 0:
-    print('Compute LEC function took ',time.clock()-time0)
-
-time0 = time.clock()
-biodiv.writeLEC('dataset/LECout.csv')
-if biodiv.rank == 0:
-    print('Output function took ',time.clock()-time0)
+    biodiv.viewResult(imName=args.output+'.png')
+    biodiv.viewElevFrequency(input=args.output+'.csv',imName=args.output+'_zfreq.png')
+    biodiv.viewLECZFrequency(input=args.output+'.csv',imName=args.output+'_leczfreq.png')
+    biodiv.viewLECFrequency(input=args.output+'.csv',imName=args.output+'_lecfreq.png')
+    biodiv.viewLECZbar(input=args.output+'.csv',imName=args.output+'_lecbar.png')
