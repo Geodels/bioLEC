e33640abdfc3f2e5cf4cce3647ac9c54bc11ad20
Tristan <tristan.salles@sydney.edu.au>
2019-08-18 16:34:11 +1000

    add logo!

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -18,6 +18,17 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../src'))
 
+import sphinx_rtd_theme
+
+from sphinx.builders.html import (StandaloneHTMLBuilder, DirectoryHTMLBuilder,
+                                  SingleFileHTMLBuilder)
+
+# Redefine supported_image_types for the HTML builder
+html_img_types = ['image/gif', 'image/svg+xml', 'image/png', 'image/jpeg']
+StandaloneHTMLBuilder.supported_image_types = html_img_types
+DirectoryHTMLBuilder.supported_image_types = html_img_types
+SingleFileHTMLBuilder.supported_image_types = html_img_types
+
 
 # -- Project information -----------------------------------------------------
 
@@ -40,11 +51,10 @@ release = u'0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode',
-              'sphinxcontrib.napoleon', 'sphinx.ext.autodoc']
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['ntemplates']
+extensions = ['sphinx.ext.viewcode', 'sphinx.ext.todo',
+              'sphinxcontrib.napoleon', 'sphinx.ext.autodoc',
+              'sphinxemoji.sphinxemoji']
+numfig = True
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -65,11 +75,16 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
+pygments_style = 'monokai'
+# pygments_style = 'default'
+sphinxemoji_style = 'twemoji'
 
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -78,6 +93,13 @@ pygments_style = None
 #
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_logo = 'logo/logo.jpg'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
